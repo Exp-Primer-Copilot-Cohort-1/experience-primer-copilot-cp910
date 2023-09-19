4108cf10c84366baaed50d6bc103becd9eb5b21e
Chaitanya Parwate <114334171+cp910@users.noreply.github.com>
2023-09-19 13:12:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,32 @@
 // Create web server
 // Run: node comments.js
-// View at http://localhost:8080
-// Note: requires express and body-parser node modules
-var express = require("express");
-var bodyParser = require("body-parser");
+// Test: curl -X GET http://localhost:3000/comments
+
+var express = require('express');
 var app = express();
-var comments = [];
-app.use(express.static("."));
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.get("/comments", function(req, res) {
-  res.json(comments);
+
+app.get('/comments', function(req, res) {
+  fs.readFile(__dirname + '/comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
-app.post("/comments", function(req, res) {
-  comments.push(req.body);
-  res.json(comments);
+
+app.post('/comments', function(req, res) {
+  fs.readFile(__dirname + '/comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
 });
-app.listen(8080);
-console.log("Server is running on port 8080");
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000');
